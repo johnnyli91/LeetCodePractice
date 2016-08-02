9a5ba77b613b8e6edb289ff6be53bd30f99b6deb
Johnny Li <johnnyli91@users.noreply.github.com>
2016-08-02 12:48:45 -0700

    [136. Single Number][Accepted]committed by LeetCode Extension

---
+++ 136-Single-Number/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        numbers_map = {}
+        for num in nums:
+            try:
+                numbers_map[num] += 1
+            except KeyError:
+                numbers_map[num] = 1
+        for key in numbers_map:
+            if numbers_map[key] == 1:
+                return key
