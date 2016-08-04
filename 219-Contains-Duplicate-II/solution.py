5c0973dba0f05768910aff053b6243f4beeb9d44
Johnny Li <johnnyli91@users.noreply.github.com>
2016-08-04 11:55:21 -0700

    [219. Contains Duplicate II][Accepted]committed by LeetCode Extension

--- 219-Contains-Duplicate-II/solution.py
+++ 219-Contains-Duplicate-II/solution.py
@@ -8,21 +8,12 @@ class Solution(object):
         numbers_map = {}
         for index, num in enumerate(nums):
             try:
-                numbers_map[num].append(index)
+                if index - numbers_map[num] <= k:
+                    return True
+                else:
+                    numbers_map[num] = index
             except KeyError:
-                numbers_map[num] = [index]
-        for key in numbers_map:
-            number_of_indices = len(numbers_map[key])
-            if number_of_indices > 1:
-                index_pointer = number_of_indices - 1
-                prev_index_pointer = index_pointer - 1
-                while index_pointer > 0:
-                    difference = numbers_map[key][index_pointer] - numbers_map[key][prev_index_pointer]
-                    if difference <= k:
-                        return True
-                    else:
-                        index_pointer -= 1
-                        prev_index_pointer = index_pointer - 1
+                numbers_map[num] = index
         return False
                         
                 
